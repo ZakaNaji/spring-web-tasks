964e8dcf52e5aa3be6157dae6445b6f238388ee4
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-30 13:29:05 +0100

    #08: in this example we create our own custum validator, our valirdator bean must impl Validator(logic of validation here), and use @InitBinder and add our validator bean to WebDataBinder (setValidator or addValidators).

--- src/main/java/com/znaji/springwebtasks/court/controller/ReservationFormController.java
+++ src/main/java/com/znaji/springwebtasks/court/controller/ReservationFormController.java
@@ -2,11 +2,15 @@ package com.znaji.springwebtasks.court.controller;
 
 import com.znaji.springwebtasks.court.demain.Player;
 import com.znaji.springwebtasks.court.demain.Reservation;
+import com.znaji.springwebtasks.court.demain.ReservationValidation;
 import com.znaji.springwebtasks.court.demain.SportType;
 import com.znaji.springwebtasks.court.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +22,7 @@ import java.util.List;
 public class ReservationFormController {
 
     private final ReservationService reservationService;
+    public final ReservationValidation reservationValidation;
 
     @ModelAttribute("sportTypes")
     public List<SportType> populateSportTypes() {
@@ -34,7 +39,10 @@ public class ReservationFormController {
     }
 
     @PostMapping
-    public String submitForm(@ModelAttribute("reservation") Reservation reservation) {
+    public String submitForm(@ModelAttribute("reservation") @Validated Reservation reservation, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "reservationForm";
+        }
         reservationService.make(reservation);
         return "redirect:/reservationForm/reservationSuccess";
     }
@@ -42,5 +50,10 @@ public class ReservationFormController {
     @GetMapping("reservationSuccess")
     public String success() {
         return "reservationSuccess";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(reservationValidation);
     }
 }
